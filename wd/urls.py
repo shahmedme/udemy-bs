5900dfab36d1807f5e9246ac63a2fbabbe99f90c
shakilahmmeed <shakilahmmeed@gmail.com>
2020-07-27 08:39:44 +0600

    azure storage setup

--- wd/urls.py
+++ wd/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', frontend_views.index)
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
